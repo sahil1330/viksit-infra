c2d4bf0da9ed5cc15bcf300994f5e741a4a9f2c3
harsh-gupta-10 <harshgupta24716@gmail.com>
2024-08-29 08:41:23 +0530

    upadted foooter

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <div class="container my-4">
         <div class="row mb-4">
             <div class="col-md-6 ">
+
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
                         <div class="col-md-4">
@@ -35,6 +36,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="col-md-6 ">
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
@@ -54,6 +56,7 @@
                 </div>
             </div>
         </div>
+
         <div class="row mb-4">
             <div class="col-md-6 ">
                 <div class="card mb-3" style="max-width: 540px;">
@@ -73,6 +76,7 @@
                     </div>
                 </div>
             </div>
+
             <div class="col-md-6 ">
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
@@ -92,6 +96,7 @@
                 </div>
             </div>
         </div>
+
         <div class="row mb-4">
             <div class="col-md-6 ">
                 <div class="card mb-3" style="max-width: 540px;">
@@ -106,11 +111,13 @@
                                     lead-in to additional content. This content is a little bit longer.</p>
                                 <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
                                 </p>
+                                <h6 class="card-text text-end">Location</h6>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
             <div class="col-md-6 ">
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
@@ -129,6 +136,47 @@
                     </div>
                 </div>
             </div>
+
+            <div class="col-md-6 ">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Blog title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 ">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Blog title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php include 'components/footer.php'; ?>
+
         </div>
     </div>
 </body>
